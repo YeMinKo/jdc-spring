436774f03643e85a054ad36a6d3d6d7157df3b6b
Ye Min Ko <yeminko47@gmail.com>
2022-01-11 20:59:34 +0630

    [added] delecatingFilterProxy demo project

---
+++ intelliJ-workspace/security-filter-chain-demo/src/main/java/com/example/securityfilterchaindemo/security/SecurtyRoles.java
@@ -0,0 +1,5 @@
+package com.example.securityfilterchaindemo.security;
+
+public class SecurtyRoles {
+    static String ADMIN = "ADMIN";
+}
---
+++ intelliJ-workspace/security-filter-chain-demo/src/test/java/com/example/securityfilterchaindemo/SecurityFilterChainDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.securityfilterchaindemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityFilterChainDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
