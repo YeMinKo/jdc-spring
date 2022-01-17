1ba3c921a2d9a0861cf8fa712ae156a1825eaefd
Ye Min Ko <yeminko47@gmail.com>
2022-01-17 19:06:48 +0630

    [added] securityfilterchain demo project

--- intelliJ-workspace/csrf-demo/src/main/java/com/example/csrfdemo/controller/TestController.java
+++ intelliJ-workspace/csrf-demo/src/main/java/com/example/csrfdemo/controller/TestController.java
@@ -1,11 +1,19 @@
 package com.example.csrfdemo.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
+
+    private static Logger logger = LoggerFactory.getLogger(TestController.class);
+
+    @Value("${test.hello}")
+    private String test;
 
     @PostMapping("/a")
     public String postEndPointA(){
@@ -14,6 +22,7 @@ public class TestController {
 
     @GetMapping("/a")
     public String getEndPointA(){
+        logger.info("TEST: " + test);
         return "Works";
     }
 
--- intelliJ-workspace/security-filter-chain-demo/src/main/java/com/example/securityfilterchaindemo/security/WebSecurityConfig.java
+++ intelliJ-workspace/security-filter-chain-demo/src/main/java/com/example/securityfilterchaindemo/security/WebSecurityConfig.java
@@ -16,7 +16,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .authorizeHttpRequests()
+                .authorizeRequests()
                 .mvcMatchers("/", "/home").permitAll()
                 .mvcMatchers("/secret-resources").hasRole(ADMIN)
                 .anyRequest().authenticated()
