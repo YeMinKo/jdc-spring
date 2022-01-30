57303674f3902dc401119287ced84b0346acf8c6
Ye Min Ko <yeminko47@gmail.com>
2022-01-30 23:25:07 +0630

    [added] microservice with eureka server

---
+++ intelliJ-workspace/rest-statelessness/counter-service/src/main/java/com/example/counterservice/ds/CounterServiceResponse.java
@@ -0,0 +1,15 @@
+package com.example.counterservice.ds;
+
+public class CounterServiceResponse {
+
+    private int number;
+
+    public CounterServiceResponse(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+}
+
